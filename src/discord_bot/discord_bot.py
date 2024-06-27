8f1c65e372ad3abe1c8e8e12915a2f44e68ef2c5
ibiboy <ibichaninter7@gmail.com>
2024-06-27 23:53:04 +0900

    概念実装

---
+++ src/discord_bot/discord_bot.py
@@ -0,0 +1,15 @@
+import discord
+
+class DiscordBot():
+    def __init__(self, token: str) -> None:
+        self.token = token
+        self.thread = None
+        self.conversation_id = None
+    def run():
+        pass
+    def new_reply():
+        pass
+    def chat(thread, conversation_id: str):
+        pass    
+    def talk():
+        pass

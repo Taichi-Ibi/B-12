c7d93ec3419df8a76a8edb84be943967f4e9cca4
ibiboy <ibichaninter7@gmail.com>
2024-06-29 12:45:32 +0900

    create discordbot class

--- src/discord_bot/discord_bot.py
+++ src/discord_bot/discord_bot.py
@@ -1,15 +1,39 @@
+from abc import ABC, abstractmethod
+
 import discord
 
-class DiscordBot():
-    def __init__(self, token: str) -> None:
+class DiscordBot(ABC):
+    def __init__(self, token):
         self.token = token
-        self.thread = None
-        self.conversation_id = None
-    def run():
+        intents = discord.Intents.default()
+        intents.message_content = True
+        self.client = discord.Client(intents=intents)
+        
+        @self.client.event
+        async def on_ready():
+            print(f'We have logged in as {self.client.user}')
+        
+        @self.client.event
+        async def on_message(message):
+            if message.author == self.client.user:
+                return
+            await self.reply(message=message)
+
+    def run(self):
+        self.client.run(self.token)
+
+    @abstractmethod
+    async def reply(self, message):
         pass
-    def new_reply():
-        pass
-    def chat(thread, conversation_id: str):
-        pass    
-    def talk():
-        pass
+
+class ThirdEye(DiscordBot):
+    async def reply(self, message):
+        thread_name = "New Thread"
+        thread = await message.create_thread(name=thread_name)
+        content = message.content
+        await thread.send(content)
+
+# 使用例
+if __name__ == "__main__":
+    bot = ThirdEye('MTI1NDAxNTk2MDQwNzM0MzE1NA.GqsmsF.sxaoQB4HS0jQcTA5oeDQeNU8uiBF-We5H_8Cp8')
+    bot.run()
